461fb7dc94ed50748a5b8418eb0ffe1a4e1f91e2
alebichir <alexandrabichir@yahoo.com>
2023-10-03 00:54:35 +0300

    slideshow gallery

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ function showContactPage() {
   activePage = "contact";
 }
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
 showHomePage();
 
 var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
@@ -72,3 +85,19 @@ studioLink.addEventListener("click", showStudioPage);
 
 var contactLink = document.querySelectorAll("#top-menu-bar a")[6];
 contactLink.addEventListener("click", showContactPage);
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
