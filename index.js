8cbcf21a502d3b2f39eab244560152cb997c6406
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-10-03 07:54:02 +0300

    improved events and add showPage(id)

--- index.js
+++ index.js
@@ -8,46 +8,10 @@ function show(id) {
   document.getElementById(id).style.display = "block";
 }
 
-function showHomePage() {
+function showPage(id) {
   hide(activePage);
-  show("home");
-  activePage = "home";
-}
-
-function showValuesPage() {
-  hide(activePage);
-  show("values");
-  activePage = "values";
-}
-
-function showAboutMePage() {
-  hide(activePage);
-  show("aboutMe");
-  activePage = "aboutMe";
-}
-
-function showAboutGalleryPage() {
-  hide(activePage);
-  show("aboutGallery");
-  activePage = "aboutGallery";
-}
-
-function showGalleryPage() {
-  hide(activePage);
-  show("gallery");
-  activePage = "gallery";
-}
-
-function showStudioPage() {
-  hide(activePage);
-  show("studio");
-  activePage = "studio";
-}
-
-function showContactPage() {
-  hide(activePage);
-  show("contact");
-  activePage = "contact";
+  show(id);
+  activePage = id;
 }
 
 let slideIndex = 1;
@@ -63,33 +27,9 @@ function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
-showHomePage();
-
-var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
-homeLink.addEventListener("click", showHomePage);
-
-var valuesLink = document.querySelectorAll("#top-menu-bar a")[1];
-valuesLink.addEventListener("click", showValuesPage);
-
-var aboutMeLink = document.querySelectorAll("#top-menu-bar a")[2];
-aboutMeLink.addEventListener("click", showAboutMePage);
-
-var aboutGalleryLink = document.querySelectorAll("#top-menu-bar a")[3];
-aboutGalleryLink.addEventListener("click", showAboutGalleryPage);
-
-var galleryLink = document.querySelectorAll("#top-menu-bar a")[4];
-galleryLink.addEventListener("click", showGalleryPage);
-
-var studioLink = document.querySelectorAll("#top-menu-bar a")[5];
-studioLink.addEventListener("click", showStudioPage);
-
-var contactLink = document.querySelectorAll("#top-menu-bar a")[6];
-contactLink.addEventListener("click", showContactPage);
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -101,3 +41,11 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
+
+showPage(activePage);
+
+document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+  var id = e.target.dataset.page;
+  console.info("click on menu bar", id);
+  showPage(id);
+});
